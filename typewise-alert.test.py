038808802f53cc7274de0b7cfede5f978898bff1
bijayv <nrbijay52@gmail.com>
2024-08-24 19:34:10 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -4,7 +4,7 @@ from typewise_alert import infer_breach
 
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+    self.assertTrue(infer_breach(20, 50, 100) == 'TOO_LOW')
 
 
 if __name__ == '__main__':
